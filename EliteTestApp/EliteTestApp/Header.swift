dfc3f37f10109ba041d84c984d518d7e1d4558cd
Chen <1582151629@qq.com>
2018-06-10 00:10:11 -0500

    Add files via upload
    first commit

---
+++ EliteTestApp/EliteTestApp/Header.swift
@@ -0,0 +1,16 @@
+//
+//  Header.swift
+//  iTunesShow
+//
+//  Created by Xin Chen on 2018/4/23.
+//  Copyright © 2018年 Xin Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+let SCREEN_HEIGH = UIScreen.main.bounds.size.height
+
+
---
+++ EliteTestApp/EliteTestApp/View/TableViewCell/VisitWebsiteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VisitWebsiteTableViewCell.swift
+//  EliteTestApp
+//
+//  Created by Xin Chen on 2018/6/9.
+//  Copyright © 2018年 Xin Chen. All rights reserved.
+//
+
+import UIKit
+
+class VisitWebsiteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
